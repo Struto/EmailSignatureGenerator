474f235755b12fafbd8573aa009f3668366e472c
Archie Makuwa <archie@aatsol.co.za>
2015-12-10 12:58:46 +0200

    Fixed UK number issues. Currently there is no validation

--- index.php
+++ index.php
@@ -1,57 +1,61 @@
 <?php
+
 if($_POST['submit']){
+
+    // Declare variables
     $name = $_REQUEST['name'];
     $position = $_REQUEST['position'];
-    $email =  $_REQUEST['email'];
     $skype =  $_REQUEST['skype'];
-    $phone= $_REQUEST['phone'];
+    $telephone= $_REQUEST['telephone'];
     $mobile =  $_REQUEST['mobile'];
 
 
-    //if ($phone == ""){
-    //    $phone = "4241 6160";
-    //}
+    // Init contact variables
+    $contactNumbersT = "";
+    $contactNumbersM = "";
+    $skypeUserID = "";
 
-    $contactInformationRows = "";
-    $emptyEndRows = 0;
-    if(strlen($_REQUEST['email']) == 0){
+
+    // If UK - Get Telephone & Mobile Number
+    if( strlen($_REQUEST['telephone']) == 0 ) {
         $emptyEndRows++;
-    }else{
-        //$contactInformationRows .= "<tr><td style=\"white-space:pre;\">E-mail:</td><td><a style=\"text-decoration: none; color: black;\" href=\"mailto:".$email."\">".$email."</a></td></tr>\n";
-        $contactInformationRows .= "<a href=\"mailto:".$email."\" style=\"color:black!important;text-decoration:none!important;\">".$email."</a><br/>\n";
-    }
-
-    if(strlen($_REQUEST['skype']) == 0){
-        $emptyEndRows++;
-    }else{
-        //$contactInformationRows .= "<tr><td style=\"white-space:pre;\">Skype:</td><td>".$skype."</td></tr>\n";
-        $contactInformationRows .= "S: ".$skype."<br/>\n";
-    }
-
-    // If UK
-    if ( (!empty($phone)) && (!empty($mobile)) ) {
-        $contactInformationRows .= "T: ".$phone."<br/>\n";
-        $contactInformationRows .= "P: ".$mobile."<br/>\n";
     } else {
-        $emptyEndRows++;
+        $contactNumbersT .= "T: ".$telephone."<br/>\n";
     }
 
-    //$contactInformationRows .= "<tr><td style=\"white-space:pre;\">Telefon:</td><td><a style=\"text-decoration: none; color: black;\" href=\"tel:".$tele."\">".$tele."</a></td></tr>\n";
-    //$contactInformationRows .= "Phone: +" . $phone . "<br/>\n";
+    if( strlen($_REQUEST['mobile']) == 0 ) {
+        $emptyEndRows++;
+    } else {
+        $contactNumbers .= "M: ".$mobile."<br/>\n";
+    }
 
+
+
+    // Get Skype ID
+    if(strlen($_REQUEST['skype']) == 0) {
+        $emptyEndRows++;
+    }else{
+        $skypeUserID .= "S: ".$skype."<br/>\n";
+    }
+
+
+    // Generate signature
     $infoToPrint = "<br/>\n<br/>\n<div style=\"font-family:Arial!important; font-size: 12px;color: black;\">\n";
     $infoToPrint .= "Many thanks,<br/>\n<br/>\n";
     $infoToPrint .= "--" . "<br/>\n";
     $infoToPrint .= "<strong style=\"color:black !important;\">".$name."</strong><br/>\n";
     $infoToPrint .= $position."<br/>\n";
     $infoToPrint .= "Struto Limited.<br/>\n";
-    $infoToPrint .= $contactInformationRows;
+    $infoToPrint .= $contactNumbersT;
+    $infoToPrint .= $contactNumbersM;
+    $infoToPrint .= $skypeUserID;
+    /* REgistered Address & Website Disabled --
     $infoToPrint .= "Web: <a href=\"http://www.struto.co.uk\" style=\"color:black!important;\">www.struto.co.uk</a><br/>\n";
     $infoToPrint .= "<br/>Struto Ltd | Registered in England & Wales Number: 07587184\n";
     $infoToPrint .= "<br/>Registered Office: Groundfloor, 1-7 Station Road, Crawley, West Sussex, RH10 1HT\n";
     $infoToPrint .= "<br/>Vat Number :151507730.<br/>\n";
     $infoToPrint .= "<br/><a style=\"text-decoration:none !important;color:black !important;\" href=\"http://www.struto.co.uk/blog\">Sign up for our <span style=\"text-decoration:underline !important;\">fantastic newsletter</span> - click here!</a><br/>\n";
-
+    */
     $infoToPrint .= "</div>";
 }
 ?>
@@ -87,14 +91,12 @@ if($_POST['submit']){
                     <input type="text" id="name" name="name" placeholder="(e.g. John Smith)" value="<?=$name?>"/><br/>
                     <label for="position">Position </label>
                     <input type="text" id="position" name="position" placeholder="(e.g. Developer)" value="<?=$position?>"/><br/>
-                    <label for="email">Email </label>
-                    <input type="email" id="email" name="email" placeholder="(e.g. john@company.com)" value="<?=$email?>"/><br/>
                     <label for="skype">Skype ID</label>
                     <input type="text" id="skype" name="skype" placeholder="(e.g. BigAce)" value="<?=$skype?>"/><br/>
                     <!-- SA guys ignore -->
                     <p style="color:red; font-size:14px;">South Africa crew, please ignore the following fields.</p>
                     <label for="telephone">Telephone (International Format without '+')</label>
-                    <input type="tel" id="telephone" name="telephone" placeholder="(e.g 27 21 556 2323)" value="<?=$phone?>"/><br/>
+                    <input type="tel" id="telephone" name="telephone" placeholder="(e.g 27 21 556 2323)" value="<?=$telephone?>"/><br/>
                     <label for="mobile">Mobile Number (International Format without '+')</label>
                     <input type="tel" id="mobile" name="mobile" placeholder="(e.g 27 21 556 2323)" value="<?=$mobile?>"/><br/>
                     <input type="submit" name="submit" value="Gimme My Signature"/>
